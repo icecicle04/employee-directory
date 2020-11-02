6f6178618e4e3cf9ac0505e995c564180aa04ca5
AK <icecicle04@gmail.com>
2020-11-01 20:07:37 -0500

    added api and pulled results

--- employee_directory/src/components/Table/index.js
+++ employee_directory/src/components/Table/index.js
@@ -2,6 +2,17 @@ import React from "react";
 import "./style.css";
 
 function Table(props) {
-  return <div className="table"></div>;
+  console.log(props);
+  return (
+    <div className="table">
+      {props.employees !== undefined ? (
+        props.employees.map((employee) => {
+          <h2>{employee.employee_name}</h2>;
+        })
+      ) : (
+        <h3>Nobody Works Here</h3>
+      )}
+    </div>
+  );
 }
 export default Table;
--- employee_directory/src/pages/List.js
+++ employee_directory/src/pages/List.js
@@ -4,23 +4,52 @@ import API from "../utils/API";
 
 class List extends Component {
   state = {
+    ascending: true,
     search: "",
-    employees: [],
-    results: [],
+    result: [],
     error: "",
   };
 
   // when the component mounts, get a list of all ee's
   componentDidMount() {
     API.getAllEmployees()
-      .then((res) => this.setState({ employees: res.data.message }))
+      .then((res) => {
+        console.log(res.data.data);
+        this.setState({ employees: res.data.data });
+      })
       .catch((err) => console.log(err));
   }
+
+  handleInputChange = (event) => {
+    // getting whatever triggered change
+    let value = event.target.value;
+    const name = event.target.name;
+
+    this.filterSearch(value);
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  searchEmployee = (query) => {
+    API.search(query).then((res) =>
+      this.setState({
+        result: res.data.results,
+        alteredResult: res.data.results,
+      })
+    );
+  };
+
+  //   const ascendingOrder = this.state.result.sort(compare);
+  //   this.setState({
+  //       ascending: !this.state.ascending,
+  //       alteredResult: ascendingOrder,
+  //   })
   render() {
     return (
       <div>
-        <Table />
-        {/* /* <SearchResults results={this.state.results} /> */}
+        <Table employees={this.state.employees} />
+        {/* <SearchResults results={this.state.results} /> */}
       </div>
     );
   }
