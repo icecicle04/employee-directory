7bee3ad32080b36bfafa9adc97f5fbf378708805
AK <icecicle04@gmail.com>
2020-11-04 21:48:02 -0500

    imported bootstrap

--- employee_directory/src/components/Table/index.js
+++ employee_directory/src/components/Table/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./style.css";
-import Table from "react-bootstrap/Table";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// import Table from "react-bootstrap/Table";
 
 function Table(props) {
   console.log(props);
@@ -10,10 +12,21 @@ function Table(props) {
         props.employees.map((employee) => {
           return (
             <wrapper>
-              <table className="">
-                <p>{employee.employee_name}</p>
-                <p>{employee.employee_salary}</p>
-              </table>
+              <div className="table ">
+                <thead>
+                  <tr>
+                    <th className="col-sm-2">
+                      {/* <td>{props.employees}</td> */}
+                      <td>{props.employees_name}</td>
+                      <td>{props.employee_salary}</td>
+                      {/* <td>{props.employee}</td> */}
+                    </th>
+
+                    <th>{props.employees_name}</th>
+                    <th>{props.employee_salary}</th>
+                  </tr>
+                </thead>
+              </div>
             </wrapper>
           );
         })
